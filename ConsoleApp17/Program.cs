76942814808fefd881b0c242e886f8ab77e499bc
nbarba00 <115491497+nbarba00@users.noreply.github.com>
2023-03-15 17:44:49 +0100

    Update Program.cs

--- ConsoleApp17/Program.cs
+++ ConsoleApp17/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp17
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
-           
+           Console.WriteLine("Hello World");
         }
     }
 }
